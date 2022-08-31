da6453d384c4438201185d2dae00a4af36989415
Leo Chen <lchen@equuscs.com>
2022-08-31 21:23:01 +0800

    Add Microsoft.EntityFrameworkCore.InMemory

--- Src/Program.cs
+++ Src/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebCoreTest.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ToDoContext>(options =>
+    options.UseInMemoryDatabase("db"));
+
 
 var app = builder.Build();
 
