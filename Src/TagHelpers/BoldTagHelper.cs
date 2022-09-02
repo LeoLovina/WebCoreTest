4df9e1dd90c414142f43c377d7b977cc27823889
Leo Chen <lchen@equuscs.com>
2022-09-02 22:00:33 +0800

    RemoveAll, PreContent.SetHtmlContent and PostContent.SetHtmlContent

---
+++ Src/TagHelpers/BoldTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebCoreTest.TagHelpers
+{
+    [HtmlTargetElement("bold")]
+    // only targets HTML markup that provides an attribute name of "bold"
+    [HtmlTargetElement(Attributes = "bold")]
+    public class BoldTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll("bold");
+            output.PreContent.SetHtmlContent("<strong>");
+            output.PostContent.SetHtmlContent("</strong>");
+        }
+    }
+}
