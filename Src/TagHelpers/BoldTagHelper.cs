a246d7772b24d9257ce65b3690b9cb3cf9c72dbc
Leo Chen <lchen@equuscs.com>
2022-09-03 15:13:25 +0800

    RemoveAll, PreContent.SetHtmlContent and PostContent.SetHtmlContent

--- Src/TagHelpers/BoldTagHelper.cs
+++ Src/TagHelpers/BoldTagHelper.cs
@@ -2,8 +2,13 @@
 
 namespace WebCoreTest.TagHelpers
 {
+    // target all "bold" HTML markup
+    // <bold><strong> Is this bold?</strong></bold>
     [HtmlTargetElement("bold")]
+
     // only targets HTML markup that provides an attribute name of "bold"
+    // Before: <p bold>Use this area to provide additional information.</p>
+    // After: <p> <strong>Use this area to provide additional information.</strong></p>
     [HtmlTargetElement(Attributes = "bold")]
     public class BoldTagHelper : TagHelper
     {
